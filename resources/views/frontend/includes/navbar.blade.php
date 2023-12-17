71e24264ca53460b97d48c308045b3525b930f6b
Dream Girl <mdshakilpatwary439@gmail.com>
2023-12-17 18:59:01 +0600

    nav active

--- resources/views/frontend/includes/navbar.blade.php
+++ resources/views/frontend/includes/navbar.blade.php
@@ -13,7 +13,7 @@ $categories = Category::where('cat_status', 1)->get();
 						<li class="{{ Route::is('frontend_site*') ? 'active' : '' }}"><a href="{{url('/')}}">Home</a></li>
 						<li class="{{ Route::is('show.all.product*') ? 'active' : '' }}"><a href="{{route('show.all.product')}}">All Product</a></li>
 						@foreach($categories as $category)
-						<li class="{{ Route::is('show.category.product*') ? 'active' : '' }}"><a href="{{route('show.category.product',$category->id)}}">{{$category->cat_name}}</a></li>
+						<li class="{{ request()->is('show/category/product/' . $category->id) ? 'active' : '' }}"><a href="{{route('show.category.product',$category->id)}}">{{$category->cat_name}}</a></li>
 						@endforeach
 
 					</ul>
